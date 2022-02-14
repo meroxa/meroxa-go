2da2e3a247a752765f2f70310954d161c816fb9a
Owen Ou <o@owenou.com>
2022-02-14 14:10:23 -0800

    Add GetFunctionLogs
    Add GetFunctionLogs

--- pkg/meroxa/log_test.go
+++ pkg/meroxa/log_test.go
@@ -30,3 +30,26 @@ func TestGetConnectorLogs(t *testing.T) {
 		t.Fatalf("mismatched of log message: want=%s got=%s", want, got)
 	}
 }
+
+func TestGetFunctionLogs(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		if want, got := "/v1/functions/my-func/logs", req.URL.Path; want != got {
+			t.Fatalf("mismatched of request path: want=%s got=%s", want, got)
+		}
+
+		w.Write([]byte("[timestamp] log message"))
+	}))
+	defer server.Close()
+
+	c := testClient(server.Client(), server.URL)
+
+	resp, err := c.GetFunctionLogs(context.Background(), "my-func")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if want, got := "[timestamp] log message", string(b); want != got {
+		t.Fatalf("mismatched of log message: want=%s got=%s", want, got)
+	}
+}
