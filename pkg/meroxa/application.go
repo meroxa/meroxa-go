0d80e029000de30cd32c0a93679b21b45c7a1908
Janelle Tavares <janelletavares@users.noreply.github.com>
2022-03-08 16:17:05 -0800

    Accept pipeline identifier when creating an application (#101)
    * Accept pipeline identifier when creating an application; Add git SHA to Application

--- pkg/meroxa/application.go
+++ pkg/meroxa/application.go
@@ -21,6 +21,7 @@ type Application struct {
 	UUID       string             `json:"uuid"`
 	Name       string             `json:"name"`
 	Language   string             `json:"language"`
+	GitSha     string             `json:"git_sha"`
 	Status     ApplicationStatus  `json:"status,omitempty"`
 	Connectors []EntityIdentifier `json:"connectors,omitempty"`
 	Functions  []EntityIdentifier `json:"functions,omitempty"`
@@ -32,8 +33,10 @@ type Application struct {
 
 // CreateApplicationInput represents the input for a Meroxa Application create operation in the API
 type CreateApplicationInput struct {
-	Name     string `json:"name"`
-	Language string `json:"language"`
+	Name     string           `json:"name"`
+	Language string           `json:"language"`
+	GitSha   string           `json:"git_sha"`
+	Pipeline EntityIdentifier `json:"pipeline"`
 }
 
 type ApplicationStatus struct {
