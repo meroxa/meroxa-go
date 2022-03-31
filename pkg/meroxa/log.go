76bdd5411f553cb55fa22aeb4fad4705b37caf81
Eric Cheatham <eric@meroxa.io>
2022-03-31 16:24:32 -0500

    Add GetBuildLogs function

--- pkg/meroxa/log.go
+++ pkg/meroxa/log.go
@@ -9,6 +9,7 @@ import (
 const (
 	connectorLogsBasePath = "/v1/connectors"
 	functionLogsBasePath  = "/v1/functions"
+	buildLogsBasePath     = "/v1/builds"
 )
 
 func (c *client) GetConnectorLogs(ctx context.Context, nameOrID string) (*http.Response, error) {
@@ -28,6 +29,21 @@ func (c *client) GetConnectorLogs(ctx context.Context, nameOrID string) (*http.R
 
 func (c *client) GetFunctionLogs(ctx context.Context, nameOrUUID string) (*http.Response, error) {
 	path := fmt.Sprintf("%s/%s/logs", functionLogsBasePath, nameOrUUID)
+
+	req, err := c.newRequest(ctx, http.MethodGet, path, nil, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	// Override content-type and accept headers to text/palin
+	req.Header.Add("Content-Type", textContentType)
+	req.Header.Add("Accept", textContentType)
+
+	return c.httpClient.Do(req)
+}
+
+func (c *client) GetBuildLogs(ctx context.Context, nameOrUUID string) (*http.Response, error) {
+	path := fmt.Sprintf("%s/%s/logs", buildLogsBasePath, nameOrUUID)
 
 	req, err := c.newRequest(ctx, http.MethodGet, path, nil, nil)
 	if err != nil {
