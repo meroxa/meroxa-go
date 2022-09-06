3bd80916eba4835df18bf1d3da8a3c8bfd2ae4a2
Raúl Barroso <ra.barroso@gmail.com>
2022-09-06 16:52:35 +0200

    feat: Add createDeployment endpoint

--- pkg/meroxa/deployment.go
+++ pkg/meroxa/deployment.go
@@ -1,6 +1,10 @@
 package meroxa
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
 	"time"
 
 	"github.com/volatiletech/null/v8"
@@ -30,4 +34,57 @@ type Deployment struct {
 	State           DeploymentStatus `json:"state"`
 	Spec            null.String      `json:"spec,omitempty"`
 	SpecVersion     null.String      `json:"spec_version,omitempty"`
+}
+
+type CreateDeploymentInput struct {
+	GitSha      string           `json:"git_sha"`     // this could be omitted.
+	Application EntityIdentifier `json:"application"` // this could be omitted.
+	Spec        null.String      `json:"spec,omitempty"`
+	SpecVersion null.String      `json:"spec_version,omitempty"`
+}
+
+func (c *client) GetLatestDeployment(ctx context.Context, name string) (*Deployment, error) {
+	resp, err := c.MakeRequest(ctx, http.MethodGet, fmt.Sprintf("%s/%s/deployments/latest", applicationsBasePath, name), nil, nil, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	err = handleAPIErrors(resp)
+	if err != nil {
+		return nil, err
+	}
+
+	var d *Deployment
+	err = json.NewDecoder(resp.Body).Decode(&d)
+	if err != nil {
+		return nil, err
+	}
+
+	return d, nil
+}
+
+func (c *client) CreateDeployment(ctx context.Context, input *CreateDeploymentInput) (*Application, error) {
+	appIdentifier, err := input.Application.GetNameOrUUID()
+
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := c.MakeRequest(ctx, http.MethodPost, fmt.Sprintf("%s/%s/deployments", applicationsBasePath, appIdentifier), input, nil, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	err = handleAPIErrors(resp)
+	if err != nil {
+		return nil, err
+	}
+
+	var a *Application
+	err = json.NewDecoder(resp.Body).Decode(&a)
+	if err != nil {
+		return nil, err
+	}
+
+	return a, nil
 }
